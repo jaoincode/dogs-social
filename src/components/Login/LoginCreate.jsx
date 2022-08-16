523e979937b41abb6f30c6ba8a865c86b8c94275
João Vitor <joaovitorg1412@outlook.com>
2022-08-16 16:40:25 -0300

    fixing "useForm" import

--- src/components/Login/LoginCreate.jsx
+++ src/components/Login/LoginCreate.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Input } from '../Forms/Input';
 import { Button } from '../Forms/Button';
 import { Error } from '../Helper/Error';
-import { useForm } from '../../Hooks/useForm';
-import { USER_POST } from '../../Api';
+import { useForm } from '../../hooks/useForm';
+import { USER_POST } from '../../api';
 import { UserContext } from '../../UserContext';
-import { useFetch } from '../../Hooks/useFetch';
+import { useFetch } from '../../hooks/useFetch';
 import { Head } from '../Helper/Head';
 
 export const LoginCreate = () => {
